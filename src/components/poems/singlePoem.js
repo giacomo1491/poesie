97f5f6c51cc67ba289a4115793ed2ae716c6cba1
giacomo <tolarigiacomo@gmail.com>
2021-04-12 16:06:49 +0200

    add json \n

--- src/components/poems/singlePoem.js
+++ src/components/poems/singlePoem.js
@@ -1,8 +1,10 @@
 function SinglePoem(props) {
+  const formattedText = props.text.replaceAll('\n','<br />');
   return (
     <div className="singlepoem--wrapper">
       <h4>{props.title}</h4>
-      <p>{props.text}</p>
+      <p><span dangerouslySetInnerHTML={{ __html: formattedText }} /></p>
+      
     </div>
   );
 }

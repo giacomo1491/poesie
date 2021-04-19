41710eb321b97c6c32e9d8e88fa30f3976e2ed96
giacomotolari <tolarigiacomo@gmail.com>
2021-04-19 13:30:37 +0200

    add photos

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ function Header() {
       </div>
       <div className='header--slide2'>
         <Nav />
-        <p className='header-text'>
+        {/* <p className='header-text'>
           “Crea parole <br /> Le parole sono vibrazioni della natura <br /> Così
           le belle parole creano una bella natura <br /> Questa è la radice
           dell'universo”
@@ -21,7 +21,23 @@ function Header() {
           l’autrice apre la sua raccolta <br /> di poesie "
           <span className='span-title'>Al Porto"</span>, sottotitolata: di
           acque, di Terre, di Amori.
-        </p>
+        </p> */}
+        “Così le belle parole creano una bella natura” Con questa citazione di
+        Masaru Emoto, l’autrice apre la sua raccolta di poesie &quot;Al
+        Porto&quot;, sottotitolata: di acque, di Terre, di Amori. La prima volta
+        che ebbi l’opportunità di leggere l’opera che affonda le radici
+        nell’atavica Terra del Sulcis, ragionai sulla veridicità della suddetta
+        dichiarazione. Ci sono poesie che ascoltiamo con piacere, altre che non
+        riusciamo a comprendere nella loro plenitudine, ci sono poesie che
+        vorremmo declamare all’occasione, infine, poesie che come la tela di un
+        pittore ci scuotono mediante un fremito di vita. Qualcuno un giorno
+        disse: “Il poeta è un pittore che dipinge con la penna”, concordo
+        pienamente e aggiungo che prima di dipingere intinge la penna nel cuore.
+        Ho letto le poesie una di seguito all’altra e mentre leggevo, nel
+        sospiro della notte, ascoltavo suoni, respiravo profumi, vedevo Cius
+        leggeri in laguna, uccelli librarsi in volo e anime raccontarsi sino a
+        mettersi a nudo. “Le belle parole” creavano “una bella natura”, quella
+        che Duilia Dias mi mostrava prendendomi per mano senza
       </div>
     </div>
   );

24cf586606b9835519835887f917aed59609a167
giacomotolari <tolarigiacomo@gmail.com>
2021-12-08 15:12:59 +0100

    add test

--- src/components/main/libro/Book.jsx
+++ src/components/main/libro/Book.jsx
@@ -12,7 +12,8 @@ function Book() {
   const [poems, setPoems] = useState([]);
   const book = useRef();
   const indexBook = useRef([]);
-  const backendUrl = process.env.REACT_APP_BACKEND_URL;
+  // const backendUrl = process.env.REACT_APP_BACKEND_URL;
+  const backendUrl = 'https://alporto.herokuapp.com/';
 
   const loadPoems = async () => {
     const response = await fetch(`${backendUrl}`);
